9ac2292ff661c899006a64b2cc3d23a8cb603c40
George <teozz89@gmail.com>
2021-05-05 10:15:34 +0800

    first-commit

---
+++ ml_proj_template.py
@@ -0,0 +1,96 @@
+# This is simple script to generate an ML-project
+import os
+
+if __name__ == '__main__':
+    author = input("Author name: ")
+    project_description = input("Project Description: ")
+    license = input("license: ")
+
+    misc_folders = [
+        os.path.join('data', 'raw'),
+        os.path.join('data', 'processed'),
+        os.path.join('models'),
+        'configs',
+        'notebooks',
+        'references',
+        'figures',
+    ]
+
+    # this folders are based on
+    # https://cloud.google.com/architecture/mlops-continuous-delivery-and-automation-pipelines-in-machine-learning
+
+    src_folders = [
+        'src',
+        os.path.join('src', 'data_extraction'),
+        os.path.join('src', 'data_validation'),
+        os.path.join('src', 'data_preprocessing'),
+        os.path.join('src', 'model_training'),
+        os.path.join('src', 'model_validation'),
+    ]
+
+    files = [
+        'requirements.txt',
+        'README.md',
+        os.path.join('configs', 'params.yaml'),
+        os.path.join('configs', 'dvc.yaml')
+
+    ]
+
+    git_ignores = '''
+# Distribution / packaging
+env/
+build/
+lib/
+lib64/
+
+# Pycharm
+.idea
+
+# VS Code
+.vscode/
+
+# Jupyter NB Checkpoints
+.ipynb_checkpoints/
+    '''
+
+    setup_template = f'''
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='{project_description}',
+    author='{author}',
+    license='{license}',
+    install_requires=[] 
+)
+    '''
+    manifiest_doc = "include *.md *.txt LICENSE"
+
+    for folder in misc_folders:
+        os.makedirs(folder, exist_ok=True)
+
+        # adding gitkeep to maintain structure
+        with open(os.path.join(folder, ".gitkeep"), 'w') as f:
+            pass
+
+    for folder in src_folders:
+        os.makedirs(folder, exist_ok=True)
+        # with open(os.path.join(folder, '.gitkeep'), 'w') as f:
+        #     pass
+        with open(os.path.join(folder, '__init__.py'), 'w') as f:
+            pass
+
+    for file in files:
+        with open(file, 'w') as f:
+            pass
+
+    with open('.gitignore', 'w') as f:
+        f.write(git_ignores)
+
+    with open('MANIFEST.in', 'w') as f:
+        f.write(manifiest_doc)
+
+    with open('setup.py', 'w') as f:
+        f.write(setup_template)
